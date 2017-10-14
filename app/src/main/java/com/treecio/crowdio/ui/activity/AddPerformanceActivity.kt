8b60391da4b73a613f97ba0f09f3741a464878aa
Juraj Mičko <micko.juraj64@gmail.com>
2017-10-14 11:57:05 +0200

    AddPerformanceActivity with fragment

---
+++ app/src/main/java/com/treecio/crowdio/ui/activity/AddPerformanceActivity.kt
@@ -0,0 +1,12 @@
+package com.treecio.crowdio.ui.activity
+
+import android.os.Bundle
+
+class AddPerformanceActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
--- app/src/main/java/com/treecio/crowdio/ui/activity/MainActivity.kt
+++ app/src/main/java/com/treecio/crowdio/ui/activity/MainActivity.kt
@@ -2,12 +2,21 @@ package com.treecio.crowdio.ui.activity
 
 import android.os.Bundle
 import com.treecio.crowdio.R
+import com.treecio.crowdio.ui.fragment.AddPerformanceFragment
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun initNew() {
+        super.initNew()
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.content, AddPerformanceFragment())
+                .commit()
     }
 
 }
