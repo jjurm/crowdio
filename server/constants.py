d38c9042768cd38d1dd7dcd5a919479d67a27aeb
Dom Jurkus <domantas.jurkus@gmail.com>
2017-10-15 00:03:36 +0100

    Hardcode jajaja

--- server/constants.py
+++ server/constants.py
@@ -6,8 +6,22 @@ state = {
 	        "lng": 2.181766,
 	        "category": "music",
 	        "praises": 5
+	    },
+	    {
+	        "id": "anotherRandomId",
+	        "lat": 41.389037,
+	        "lng": 2.183317,
+	        "category": "magic",
+	        "praises": 3
+	    },
+	    {
+	        "id": "anotherRandomId",
+	        "lat": 41.393542,
+	        "lng": 2.183993,
+	        "category": "aerobics",
+	        "praises": 3
 	    }
-	]	
+	]
 }
 
 
