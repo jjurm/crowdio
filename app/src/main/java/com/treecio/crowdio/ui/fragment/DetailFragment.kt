0109322f368f8cbe0f2e7b1ba19dac9a42ed0a58
Juraj Mičko <micko.juraj64@gmail.com>
2017-10-15 10:09:45 +0200

    Add map preview to DetailFragment

--- app/src/main/java/com/treecio/crowdio/ui/fragment/DetailFragment.kt
+++ app/src/main/java/com/treecio/crowdio/ui/fragment/DetailFragment.kt
@@ -1,28 +1,89 @@
 package com.treecio.crowdio.ui.fragment
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.treecio.crowdio.R
 import com.treecio.crowdio.model.Category
 import com.treecio.crowdio.model.Performance
+import com.treecio.crowdio.util.withNonNull
 import kotlinx.android.synthetic.main.fragment_details.view.*
+import java.util.*
+
 
 /**
  * Created by Vytautas on 10/14/17.
  */
-class DetailFragment() : Fragment() {
+class DetailFragment() : Fragment(), OnMapReadyCallback {
+
+    internal lateinit var mMapView: MapView
+    private var map: GoogleMap? = null
+    var location: LatLng? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_details, container, false)
 
+        mMapView = view.findViewById(R.id.mapView)
+        mMapView.isClickable = false
+        mMapView.onCreate(savedInstanceState)
+
+        mMapView.onResume() // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(activity.applicationContext)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        mMapView.getMapAsync(this)
+
+        mMapView.setOnClickListener {
+            withNonNull(location) { location ->
+                val uri = String.format(Locale.ENGLISH, "geo:%f,%f",
+                        location.latitude, location.latitude)
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+                context.startActivity(intent)
+            }
+        }
+
         return view
     }
 
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(map: GoogleMap?) {
+        map ?: return
+
+        withNonNull(location) { location ->
+            map.addMarker(MarkerOptions()
+                    .position(location)
+            )
+
+            val cameraPosition = CameraPosition.Builder()
+                    .target(LatLng(location.latitude, location.longitude))      // Sets the center of the map to location user
+                    .zoom(17f)                   // Sets the zoom
+                    .tilt(40f)                   // Sets the tilt of the camera to 30 degrees
+                    .build()                   // Creates a CameraPosition from the builder
+            map.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+        }
+
+        this.map = map
+    }
+
     fun showPerformance(performance: Performance) {
+
+        withNonNull(performance.lat, performance.lng) { lat, lng ->
+            location = LatLng(lat, lng)
+        }
 
         val v = view ?: return
         v.categorieTextView.setText(performance.category?.title ?: Category.other.title)
--- app/src/main/java/com/treecio/crowdio/util/Extensions.kt
+++ app/src/main/java/com/treecio/crowdio/util/Extensions.kt
@@ -6,3 +6,11 @@ import android.os.Handler
 fun Context.runOnMainThread(r: Runnable) {
     Handler(getMainLooper()).post(r)
 }
+
+fun <A, R> withNonNull(a: A?, block: (A) -> R): R? {
+    return if (a != null) block.invoke(a) else null
+}
+
+fun <A, B, R> withNonNull(a: A?, b: B?, block: (A, B) -> R): R? {
+    return if (a != null && b != null) block.invoke(a, b) else null
+}
